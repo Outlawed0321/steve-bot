001290cfe0dd158677bd85507b8e575e7fa74f94
Caleb Eaves <pcgamer0321@gmail.com>
2018-08-19 17:07:28 -0500

    Added Connection Class

---
+++ SteveBotCore/Discord/Connection.cs
@@ -0,0 +1,27 @@
+﻿using Discord.WebSocket;
+using SteveBotCore.Discord.Entities;
+using System.Threading.Tasks;
+
+namespace SteveBotCore.Discord
+{
+    public class Connection
+    {
+        private DiscordSocketClient _client;
+        private DiscordLogger _logger;
+
+        public Connection(DiscordLogger logger)
+        {
+            _logger = logger;
+        }
+
+        internal async Task ConnectAsync(SteveBotConfig config)
+        {
+            _client = new DiscordSocketClient(config.SocketConfig);
+
+            
+
+            _client.Log += _logger.Log;
+
+        }
+    }
+}
