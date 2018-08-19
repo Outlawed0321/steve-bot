84287fabfd07ce4be030bc2571b0315fe08d13eb
Caleb Eaves <pcgamer0321@gmail.com>
2018-08-19 14:13:28 -0500

    Created Unity Class

---
+++ SteveBotCore/Unity.cs
@@ -0,0 +1,37 @@
+﻿using SteveBotCore.Storage;
+using SteveBotCore.Storage.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using Unity.Lifetime;
+using Unity.Resolution;
+
+namespace SteveBotCore
+{
+    public static class Unity
+    {
+        private static UnityContainer _container;
+
+        public static UnityContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                    RegisterTypes();
+                return _container;
+            }
+        }
+
+        public static void RegisterTypes()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IDataStorage, InMemoryStorage>(new ContainerControlledLifetimeManager());
+        }
+
+        public static T Resolve<T>()
+        {
+            return (T)Container.Resolve(typeof(T), string.Empty, new CompositeResolverOverride());
+        }
+    }
+}
