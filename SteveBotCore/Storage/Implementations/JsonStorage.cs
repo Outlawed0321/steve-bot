8b52961fe3f29d1740910c897b5402aac036bb8c
Caleb Eaves <pcgamer0321@gmail.com>
2018-08-19 18:35:30 -0500

    Added Json Storage Support

---
+++ SteveBotCore/Storage/Implementations/JsonStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using static System.IO.Directory;
+
+namespace SteveBotCore.Storage.Implementations
+{
+    public class JsonStorage : IDataStorage
+    {
+        public T RestoreObject<T>(string key)
+        {
+            var json = File.ReadAllText($"{key}.json");
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public void StoreObject(object obj, string key)
+        {
+            var file = $"{key}.json";
+            CreateDirectory(Path.GetDirectoryName(file));
+            var json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(file, json);
+        }
+    }
+}
