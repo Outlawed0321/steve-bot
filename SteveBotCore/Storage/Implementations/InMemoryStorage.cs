85d48ac8c254376cfb8fb8fdc151df7c8fff5cf9
Caleb Eaves <pcgamer0321@gmail.com>
2018-08-19 14:13:10 -0500

    Created InMemoryStorage Class

---
+++ SteveBotCore/Storage/Implementations/InMemoryStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SteveBotCore.Storage.Implementations
+{
+    class InMemoryStorage : IDataStorage
+    {
+
+        private readonly Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+
+        public void StoreObject(object obj, string key)
+        {
+            if (_dictionary.ContainsKey(key)) return;
+            _dictionary.Add(key, obj);
+        }
+
+        public T RestoreObject<T>(string key)
+        {
+            if (!_dictionary.ContainsKey(key))
+                throw new ArgumentException($"The provided key '{key}' wasn't found");
+
+            return (T)_dictionary[key];
+        }
+
+    }
+}
