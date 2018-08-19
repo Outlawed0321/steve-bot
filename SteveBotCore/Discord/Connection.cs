90e64130e11d7daf6c1b1bcd7fb5dafea20fa2bf
Caleb Eaves <pcgamer0321@gmail.com>
2018-08-19 18:33:07 -0500

    Can Actually Connect To Discord Now

--- SteveBotCore/Discord/Connection.cs
+++ SteveBotCore/Discord/Connection.cs
@@ -1,4 +1,5 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 using SteveBotCore.Discord.Entities;
 using System.Threading.Tasks;
 
@@ -6,22 +7,23 @@ namespace SteveBotCore.Discord
 {
     public class Connection
     {
-        private DiscordSocketClient _client;
-        private DiscordLogger _logger;
+        private readonly DiscordSocketClient _client;
+        private readonly DiscordLogger _logger;
 
-        public Connection(DiscordLogger logger)
+        public Connection(DiscordLogger logger, DiscordSocketClient client)
         {
             _logger = logger;
+            _client = client;
         }
 
         internal async Task ConnectAsync(SteveBotConfig config)
         {
-            _client = new DiscordSocketClient(config.SocketConfig);
-
-            
-
             _client.Log += _logger.Log;
 
+            await _client.LoginAsync(TokenType.Bot, config.Token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
