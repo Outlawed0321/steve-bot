f28382b9d0eda84c636714d0747707e512782bb3
Caleb Eaves <pcgamer0321@gmail.com>
2018-08-19 16:06:53 -0500

    Added Socket Configs

---
+++ SteveBotCore/Discord/Entities/SteveBotConfig.cs
@@ -0,0 +1,10 @@
+﻿using Discord.WebSocket;
+
+namespace SteveBotCore.Discord.Entities
+{
+    class SteveBotConfig
+    {
+        public string Token { get; set; }
+        public DiscordSocketConfig SocketConfig { get; set; }
+    }
+}
---
+++ SteveBotCore/Discord/SocketConfig.cs
@@ -0,0 +1,21 @@
+﻿using Discord.WebSocket;
+using Discord;
+
+namespace SteveBotCore.Discord
+{
+    public static class SocketConfig
+    {
+        public static DiscordSocketConfig GetDefault()
+        {
+            return new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            };
+        }
+
+        public static DiscordSocketConfig GetNew()
+        {
+            return new DiscordSocketConfig();
+        }
+    }
+}
